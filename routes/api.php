7d12c4fd8dbbf9125867055dedf4c51a3639e2c8
Wanda-Wairimu <wanda@my1health.com>
2024-11-04 20:42:32 +0300

    updates

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactFormDetailsController;
 
 Route::post('/contact/submit', [ContactFormDetailsController::class, 'sendEmail']);
+
 Route::get('/contact/get-submit', [ContactFormDetailsController::class, 'getSubmittedForms']);
+
