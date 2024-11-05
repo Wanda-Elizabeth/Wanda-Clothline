d34fa4ee77d538b75e990d8b0b2de879564b894e
Wanda-Wairimu <wanda@my1health.com>
2024-11-05 21:13:46 +0300

    updates

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/{any}', function () {
     return view('welcome');
-});
-
-Route::get('/contactus', function () {
-    return view('welcome');
-});
+})->where('any', '.*');
 
 
