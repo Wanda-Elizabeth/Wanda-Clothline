2992630c2865b576e1eab99ef42ab20fc6c807e5
Wanda-Elizabeth <wandaelizabeth1334@gmail.com>
2025-01-21 13:29:17 +0300

    New updates on wanda clotheline

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,12 +2,6 @@ import { createApp } from 'vue';
 import router from "./router/index.js";
 import './assets/styles/tailwind.css';
 
-import ImagePost from './ImagePost.vue'; // Import your Vue component
-
-createApp(ImagePost).mount('#app'); // Mount the app on the <div id="app"></div>
-
-
-
 import Home from "./components/Home.vue";
 
 createApp(Home)
